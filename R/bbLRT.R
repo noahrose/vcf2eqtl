5b579c2e8c51af40e31dbc4cba42e30f64a23dc2
Noah Rose <noahhrose@gmail.com>
2017-04-10 11:46:25 -0400

    function names

--- R/bbLRT.R
+++ R/bbLRT.R
@@ -1,9 +1,8 @@
 bbLRT<-function(dat){
-	full.out<-suppressWarnings(optim(c(1,1),bbfull,data=dat))
-	null.out<-suppressWarnings(optim(c(1,1),bbnull,data=dat))
+	full.out<-suppressWarnings(optim(c(1,1),bbFull,data=dat))
+	null.out<-suppressWarnings(optim(c(1,1),bbNull,data=dat))
 	p.out<-pchisq(2*(null.out$value-full.out$value),df=1,lower.tail=F)
 	mu<-full.out$par[1]/sum(full.out$par)
 	l2fc<-log2(mu/(1-mu))
-	z=sign(0.5-mu)*qnorm(p)
-	return(c(z=z,p.value=p.out, l2fc=l2fc))
+	return(c(mu=mu,p.value=p.out, l2fc=l2fc))
 }
