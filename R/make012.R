6bb4c4a2ef9488eac0bc33b12974281a17e3c054
Noah Rose <noahhrose@gmail.com>
2017-04-19 11:49:07 -0400

    handle missing data

--- R/make012.R
+++ R/make012.R
@@ -2,7 +2,7 @@ make012 <-
 function(mat){
 	row_names=rownames(mat)
 	col_names=colnames(mat)
-	allowed=c('0|0','0/0','0/1','1/0','0|1','1|0','1/1','1|1','./.')
+	allowed=c('0|0','0/0','0/1','1/0','0|1','1|0','1/1','1|1','.')
 	if(any(!mat%in%allowed)){
 		stop(paste('unexpected genotype(s):',paste(mat[!mat%in%allowed],collapse=','),'-- are you sure these are biallelic SNPs?'))
 	}
@@ -10,7 +10,7 @@ function(mat){
 	newmat[mat=='0/0'|mat=='0|0']<-0
 	newmat[mat=='0/1'|mat=='1/0'|mat=='0|1'|mat=='1|0']<-1
 	newmat[mat=='1/1'|mat=='1|1']<-2
-	newmat[mat=='./.']<-NA
+	newmat[mat=='.']<-NA
 	newmat<-apply(newmat,2,as.numeric)
 	rownames(newmat)<-row_names
 	colnames(newmat)<-col_names
